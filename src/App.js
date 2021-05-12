24c6ab928cce7ab6b7987bf91d1b258805a7b66c
tansibMuttakin <tansib.muhaimin@northsouth.edu>
2021-05-12 17:09:04 +0600

    structured into multiple and nested components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import NewExpense from './components/NewExpense';
 
 function App() {
   const expenses = [
@@ -41,30 +41,7 @@ function App() {
         </a>
       </header>
       <h2>Lets get started</h2> */}
-      <ExpenseItem 
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      >
-      </ExpenseItem>
-      <ExpenseItem 
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      >
-      </ExpenseItem>
-      <ExpenseItem 
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      >
-      </ExpenseItem>
-      <ExpenseItem 
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      >
-      </ExpenseItem>
+      <NewExpense expenses ={expenses} ></NewExpense>
     </div>
   );
 }
